d1c3bd2d559ea4f9fa3eeb3ef1d556e59436de68
kjayvam <javasw@naver.com>
2024-01-16 09:30:03 +0900

    Security의 로그인 페이지 해결

--- planner/planner/src/main/java/com/project/planner/controller/IndexController.java
+++ planner/planner/src/main/java/com/project/planner/controller/IndexController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class IndexController {
     @GetMapping("/")
-    public String home() {
+    public String index() {
         return "index";
     }
 }
