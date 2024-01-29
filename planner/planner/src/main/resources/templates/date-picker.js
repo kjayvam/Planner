821f10bfcf7f0dd2919e2e7bd0006d54711e81ef
vwlsg <vwlsg@DESKTOP-Q7O6KRI>
2024-01-29 17:08:32 +0900

    20240129 메인 모달

---
+++ planner/planner/src/main/resources/templates/date-picker.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#datepicker").datepicker();
+});
+
+$(".clockpicker").clockpicker();
+
+$('img.ui-datepicker-trigger').css({'cursor':'pointer', 'margin-left':'5px'});  //아이콘(icon) 위치
+
+$.fn.datepicker.dates["en"] = {
+    days: ["일", "월", "화", "수", "목", "금", "토"],
+    daysShort: ["일", "월", "화", "수", "목", "금", "토"],
+    daysMin: ["일", "월", "화", "수", "목", "금", "토"],
+    months: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+    monthsShort: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+    today: "오늘",
+    clear: "Clear",
+    format: "yyyy/mm/dd",
+    titleFormat: "yyyy년 MM" /* Leverages same syntax as 'format' */,
+    weekStart: 0,
+};
