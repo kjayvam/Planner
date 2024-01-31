693f5c0b0879adc8a5448ef0ea7839b03f0ba933
kjayvam <javasw@naver.com>
2024-01-31 17:36:02 +0900

    js btn 적용

--- planner/planner/src/main/resources/static/js/btn-href-controller.js
+++ planner/planner/src/main/resources/static/js/btn-href-controller.js
@@ -8,34 +8,39 @@ function onClickEvent() {
     indexBtn.addEventListener('click', indexMove);
 
     function indexMove() {
-
         location.href = '/';
     }
-
+    btn-href-controller
     /* id 찾기 버튼 - id 찾기 이동 */
-    let idBtn = document.getElementsByName('moveIdBtn')[0];
-    idBtn.addEventListener('click', idMove);
+    let idBtns = document.getElementsByName('moveIdBtn');
+
+    for(let i = 0; i < idBtns.length; i++) {
+        idBtns[i].addEventListener('click', idMove);
+    }
 
     function idMove() {
-
         location.href = '/move/find/id';
     }
 
     /* pw 찾기 버튼 - pw 찾기 이동 */
-    let pwBtn = document.getElementsByName('movePwBtn')[0];
-    pwBtn.addEventListener('click', pwMove);
+    let pwBtns = document.getElementsByName('movePwBtn');
+
+    for(let i = 0; i < pwBtns.length; i++) {
+        pwBtns[i].addEventListener('click', pwMove);
+    }
 
     function pwMove() {
-
         location.href = '/move/find/pw';
     }
 
     /* 회원가입 버튼 - 회원가입 이동 */
-    let signupBtn = document.getElementsByName('moveSignupBtn')[0];
-    signupBtn.addEventListener('click', signupMove);
+    let signupBtns = document.getElementsByName('moveSignupBtn');
+
+    for(let i = 0; i < signupBtns.length; i++) {
+        signupBtns[i].addEventListener('click', signupMove);
+    }
 
     function signupMove() {
-
         location.href = '/move/join/member';
     }
 }
