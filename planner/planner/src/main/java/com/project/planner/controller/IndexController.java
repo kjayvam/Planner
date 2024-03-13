3c317f4ad8a85a4f1483a09d402877c5cdf5a103
kjayvam <javasw@naver.com>
2024-03-14 03:47:07 +0900

    겹치는 문제 해결

--- planner/planner/src/main/java/com/project/planner/controller/IndexController.java
+++ planner/planner/src/main/java/com/project/planner/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.project.planner.controller;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.security.Security;
 
 @Controller
 public class IndexController {
